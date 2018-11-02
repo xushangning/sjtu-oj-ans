f7def2fe52d87f81a8b820ba3b24bfded1756d68
Shining Xu <xushangning@sjtu.edu.cn>
2018-11-02 14:24:16 +0800

    Add queue.h

---
+++ src/include/queue.h
@@ -0,0 +1,26 @@
+#ifndef QUEUE_H_
+#define QUEUE_H_
+
+namespace sx
+{
+
+template <typename T, typename Container>
+class queue
+{
+protected:
+    Container c;
+public:
+    T& front() { return c.front(); }
+    const T& front() const { return c.front(); }
+    T& back() { return c.back(); }
+    const T& back() const { return c.back(); }
+    bool empty() const { return c.empty(); }
+    int size() const { return c.size(); }
+    void push(const T& value) { c.push_back(value); }
+    void push(T&& value) { c.push_back(value); }
+    void pop() { c.pop_front(); }
+};
+
+}
+
+#endif // QUEUE_H_
